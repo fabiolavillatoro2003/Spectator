95a31204a8dfd01cc56bfc3f9d0d18cc92fcfe4f
fabiolavillatoro2003 <fabiolamariavillatoro@gmail.com>
2023-02-27 17:41:02 -0500

    fixed frontend and button cursors on hover

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,60 +3,60 @@ import './App.css';
 
 function App() {
   return (
-    <div class="App">
+    <div className="App">
       <div>
-        <img src="https://images.squarespace-cdn.com/content/53bf2a9be4b05a05f0bd263c/1484560744718-DP3TUIH6L91H0AONXPRG/Screen+Shot+2017-01-16+at+4.58.38+AM.png?format=1000w&content-type=image%2Fpng">
-          alt="Spectator Publishing Company Logo" width='20%' height='20%'</img>
-        <h1 class="heading">Spectator's Sources</h1>
+        <img src="https://images.squarespace-cdn.com/content/53bf2a9be4b05a05f0bd263c/1484560744718-DP3TUIH6L91H0AONXPRG/Screen+Shot+2017-01-16+at+4.58.38+AM.png?format=1000w&content-type=image%2Fpng"
+          alt="Spectator Publishing Company Logo" width='20%' height='20%'></img>
+        <h1 className="heading">Spectator's Sources</h1>
 
-        <div class="nav">
-          <p class="nav-text">
-            <h5>Source Name:</h5>
-          </p>
-          <div class="whitebox"><input type='text'></input></div>
-          <p class="nav-text">
-            <h5>Source Email:</h5>
-          </p>
-          <div class="whitebox"><input type="text">TYPE</input></div>
-          <button class="button-green">ADD</button>
+        <div className="nav">
+          <h5 className="nav-text">
+           Source Name:
+          </h5>
+          <div className="whitebox"><input type='text' placeholder="TYPE"/></div>
+          <h5 className="nav-text">
+            Source Email:
+          </h5>
+          <div className="whitebox"><input type="text"/></div>
+          <button className="button-green">ADD</button>
         </div>
       </div>
 
-      <div class="list-item">
-        <p class="list-maintext">
+      <div className="list-item">
+        <p className="list-maintext">
           1
         </p>
-        <p class="list-maintext">
+        <p className="list-maintext">
           Presbo
         </p>
-        <p class="list-subtext">
+        <p className="list-subtext">
           presbo@columbia.edu
         </p>
-        <button class="button-red">DELETE</button>
+        <button className="button-red">DELETE</button>
       </div>
-      <div class="list-item">
-        <p class="list-maintext">
+      <div className="list-item">
+        <p className="list-maintext">
           2
         </p>
-        <p class="list-maintext">
+        <p className="list-maintext">
           John Jay Mouse
         </p>
-        <p class="list-subtext">
+        <p className="list-subtext">
           mouse@columbia.edu
         </p>
-        <button class="button-red">DELETE</button>
+        <button className="button-red">DELETE</button>
       </div>
-      <div class="list-item">
-        <p class="list-maintext">
+      <div className="list-item">
+        <p className="list-maintext">
           1
         </p>
-        <p class="list-maintext">
+        <p className="list-maintext">
           Water Bottle Man
         </p>
-        <p class="list-subtext">
+        <p className="list-subtext">
           flipper@columbia.edu
         </p>
-        <button class="button-red">DELETE</button>
+        <button className="button-red">DELETE</button>
       </div>
     </div>
   );
