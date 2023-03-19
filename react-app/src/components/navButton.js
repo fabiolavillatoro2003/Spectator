b9d8d71c4175421e3b8efec79c6a3a8508977871
fabiolavillatoro2003 <fabiolamariavillatoro@gmail.com>
2023-03-18 20:44:11 -0400

    Capitalized navButton to NavButton, seemed to give me an error before

--- react-app/src/components/navButton.js
+++ react-app/src/components/navButton.js
@@ -1,25 +1,19 @@
-function navButton(){
+function NavButton(){
     return (
         <div className="nav">
-          <h5 className="nav-text">
-           Source Name:
-          </h5>
-          <div className="whitebox"><input type='text' placeholder="TYPE"/></div>
-          <h5 className="nav-text">
+            <h5 className="nav-text">
+            Source Name:
+            </h5>
+            <div className="whitebox"><input type='text' placeholder="TYPE"/></div>
+            <h5 className="nav-text">
             Source Email:
-          </h5>
-          <div className="whitebox"><input type="text"/></div>
+            </h5>
+            <div className="whitebox"><input type="text"/></div>
           </div>
     );
 }
 
-export default navButton;
+export default NavButton;
 
-function addButton(){
-    return(
-        
-    <button className="button-green">ADD</button>
-        
-    );
-}
+
 
