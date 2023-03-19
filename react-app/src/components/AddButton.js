3495b04be4063fe938954cc77bab4b4344a04c3d
fabiolavillatoro2003 <fabiolamariavillatoro@gmail.com>
2023-03-18 20:43:20 -0400

    Made add button separate component folder, had to capitalize name to import

---
+++ react-app/src/components/AddButton.js
@@ -0,0 +1,10 @@
+function addButton(){
+    return(
+      <div className="aButton">
+        <button className="button-green">ADD</button>
+      </div> 
+        
+    );
+}
+
+export default addButton;
