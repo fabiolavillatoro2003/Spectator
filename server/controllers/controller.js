a66e1b73a2e1038424ce1afd332a319aa011536b
fabiolavillatoro2003 <fabiolamariavillatoro@gmail.com>
2023-03-26 01:32:28 -0400

    First bits of progress towards functional endpoints

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,4 +2,13 @@ module.exports = {
     hello: (req, res) => {
         res.json({"message": "Hello World!"});
     },
+    get: (req, res) => {
+        res.json({"message": "Retrieving"});
+    },
+    post: (req, res) => {
+        res.json({"message": "Added"});
+    },
+    postDelete: (req, res) => {
+        res.json({"message": "Deleted"});
+    },
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,5 +2,7 @@ const controllers = require('../controllers/controller.js');
 const router = require('express').Router();
 
 router.route("/hello").get(controllers.hello);
-
+router.route("/api/sources").get(controllers.sources);
+router.route("/api/add_sources").post(controllers.add_sources);
+router.route("/api/delete_source/:id").post(controllers.id);
 module.exports = router;
